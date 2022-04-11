6c0e07dc150e20882ded56c93bf67036453a3e43
Michael A. Pedersen <me@findmichael.dk>
2022-04-11 23:53:38 +0200

    First commit with vue

---
+++ resources/js/components/Frontpage.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app id="main">
+    <v-navigation-drawer v-model="drawer" app>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <Main />
+  </v-app>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  },
+  data: () => ({ drawer: null }),
+};
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/main')
+
+@section('content')
+    <div id="app">
+        <Frontpage />
+    </div>
+@endsection
