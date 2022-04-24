e13e248fa37b8892392d36a16a3d4ee9a5fb6e98
Michael A. Pedersen <me@findmichael.dk>
2022-04-24 12:36:39 +0200

    feat: Display data in vue

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -2,8 +2,24 @@
   <v-main>
     <v-container>
       <v-row>
-        <v-col v-for="n in 24" :key="n" cols="4">
-          <v-card height="200"></v-card>
+        <v-col v-for="task in tasks" :key="task.id" cols="4">
+          <v-card height="200">
+                <v-card-title>{{ task.name }}</v-card-title>
+                <v-row
+                    align="center"
+                    justify="space-between"
+                    class="mx-0"
+                >
+                    <div class="grey--text mx-4">
+                        {{task.udvalg}}
+                    </div>
+
+                    <div class="grey--text mx-4">
+                        {{task.start}} - {{task.end}}
+                    </div>
+                </v-row>
+                <v-card-text>{{ task.description }}</v-card-text>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
@@ -11,4 +27,22 @@
 </template>
 
 <script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    async getTasks() {
+      await axios.get("/api/tasks").then((response) => {
+        this.tasks = response.data;
+      });
+    },
+  },
+  created() {
+    this.getTasks();
+  },
+};
 </script>
